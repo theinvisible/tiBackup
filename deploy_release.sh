e776ec6d4d2a6c5172fa4bee42b6b9b647f9bdb3
Rene Hadler <rene.hadler@iteas.at>
2014-08-26 14:37:42 +0200

    Dev

--- deploy_release.sh
+++ deploy_release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm /home/rene/DEV/bin/tiBackup
-cp -a /home/rene/DEV/qtcreator/build-tiBackup-Desktop-Release/tiBackup /home/rene/DEV/bin/tiBackup
+cp -a /home/rene/DEV/qtcreator/build-tiBackup-Deskto*-Release/tiBackup /home/rene/DEV/bin/tiBackup
